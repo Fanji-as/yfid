37b9f19dc3cb6399699167c0d1e3745b03220369
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2024-03-18 11:18:33 +0700

    feat: add API endpoint to handle midtrans payment notification

--- routes/api/v1/midtrans.js
+++ routes/api/v1/midtrans.js
@@ -1,4 +1,5 @@
 import { asyncHandler } from "../../../app/supports/helpers.js";
+import crypto from "crypto";
 import dotenv from "dotenv";
 import { Router } from "express";
 import midtransClient from "midtrans-client";
@@ -19,6 +20,37 @@ router.post(
     const token = await snap.createTransaction(req.body);
 
     return res.json({ data: token });
+  }),
+);
+
+router.post(
+  "/midtrans/notification",
+  asyncHandler(async (req, res, next) => {
+    // validation using signature key
+    const hash = crypto.createHash("sha512");
+    const data = hash.update(
+      req.body.order_id +
+        req.body.status_code +
+        req.body.gross_amount +
+        process.env.MIDTRANS_SERVER_KEY,
+      "utf-8",
+    );
+    const signatureKey = data.digest("hex");
+
+    if (req.body.signature_key !== signatureKey) {
+      throw new Error("Signature is not valid.");
+    }
+
+    // validation using Midtrans API
+    const snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: process.env.MIDTRANS_SERVER_KEY,
+      clientKey: process.env.MIDTRANS_CLIENT_KEY,
+    });
+
+    const response = await snap.transaction.notification(req.body);
+
+    return res.json({ response });
   }),
 );
 
